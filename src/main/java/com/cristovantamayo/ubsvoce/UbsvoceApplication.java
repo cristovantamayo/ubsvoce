1d8146bd45651316ef0f629c0047dcaf2d99c94d
Cristovan Tamayo <cristovan.tamayo@gmail.com>
2019-11-02 14:02:31 -0300

    Criação do Projeto

---
+++ src/main/java/com/cristovantamayo/ubsvoce/UbsvoceApplication.java
@@ -0,0 +1,13 @@
+package com.cristovantamayo.ubsvoce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbsvoceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbsvoceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cristovantamayo/ubsvoce/UbsvoceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristovantamayo.ubsvoce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbsvoceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
